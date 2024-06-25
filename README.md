d24ab9c7fb339dd0d8ff7a752b6ca51f25cf7780
Albert Ribas <albert.ribas@flixbus.com>
2024-06-25 17:45:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# rpi-dynamic-dns
+
+This Python script is designed to update your DNS records with your current external IP address. It uses the Cloudflare API to fetch and update the DNS records for a specified zone.
+
+## Disclaimer
+
+This script is currently configured to only update 'A' type DNS records for a single Cloudflare zone. The zone ID is hardcoded in an environment variable, as I only have access to one Cloudflare zone at the moment. If you need to update other record types or manage multiple zones, you will need to modify the script accordingly.
+
+## Features
+
+- Retrieves the external IP address of the machine using the `ipify.org` API.
+- Fetches the existing DNS records for the specified Cloudflare zone.
+- Updates the DNS records with the new external IP address.
+- Logs all actions to a log file (`py_logs.log`) for debugging purposes.
+
+## Prerequisites
+
+- Python 3.6 or higher
+- Cloudflare API credentials (API key and zone ID)
+- Environment variables for Cloudflare credentials and zone ID
+
+## Installation
+
+1. Clone the repository:
+```sh
+git clone https://github.com/your-username/rpi-dynamic-dns.git
+```
+2. Change to the project directory:
+```sh
+cd rpi-dynamic-dns
+```
+3. Create a virtual environment and activate it:
+```sh
+python3 -m venv venv
+source venv/bin/activate
+```
+4. Install the required dependencies:
+```sh
+pip install -r requirements.txt
+```
+5. Set the environment variables for Cloudflare credentials and zone ID:
+```sh
+export CLOUDFLARE_API_KEY=your_cloudflare_api_key
+export ZONE_ID=your_cloudflare_zone_id
+```
+6. Run the script:
+```sh
+python main.py
+```
+
+## Usage
+
+The script will automatically update the DNS records with the current external IP address. You can set up a cron job or a systemd service to run the script periodically to keep the DNS records up-to-date.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Acknowledgments
+
+- The Cloudflare Python API library: https://github.com/cloudflare/python-cloudflare
+- The `ipify.org` API for retrieving the external IP address
+
---
+++ main.py
@@ -0,0 +1,108 @@
+import logging
+import os
+import urllib.request
+
+from datetime import datetime
+from dotenv import load_dotenv
+from cloudflare import Cloudflare
+from cloudflare.types.dns import ARecord, CNAMERecord
+
+logging.basicConfig(
+    filename='py_logs.log',
+    filemode="w",
+    encoding='utf-8',
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    level=logging.INFO
+)
+
+load_dotenv()
+
+
+def get_public_ip() -> str:
+    """
+    Retrieves the external IP address of the machine.
+
+    Returns:
+        str: The external IP address
+    """
+    try:
+        url = "https://api.ipify.org"  # IPv4 only
+        response = urllib.request.urlopen(url, timeout=5)
+        external_ip = response.read().decode('utf-8')
+        logging.info(f"External IP: {external_ip}")
+        return external_ip
+    except Exception as e:
+        logging.error(f"Error: {e.reason.strerror}")
+        return
+
+
+def fetch_dns_records(cf: Cloudflare, zone_id: str, type: str = 'ALL') -> list:
+    """
+    Fetches the DNS records for the specified zone.
+
+    Args:
+        cf (Cloudflare): An instance of the Cloudflare class
+        zone_id (str): The ID of the Cloudflare zone
+        record_type (str): The type of DNS records to fetch ('A' or 'CNAME',
+            defaults to 'ALL')
+
+    Returns:
+        list: A list of DNS records
+    """
+    record_types = {
+        'ALL': lambda record: True,
+        'A': lambda record: isinstance(record, ARecord),
+        'CNAME': lambda record: isinstance(record, CNAMERecord),
+    }
+    filter_func = record_types.get(type, None)
+    if filter_func is None:
+        raise ValueError(f"Invalid record type: {type}")
+
+    records = cf.dns.records.list(zone_id=zone_id)
+    logging.info(f"DNS records fetched: {len(list(records))}")
+
+    filtered_records = [record for record in records if filter_func(record)]
+    logging.info(f"DNS records filtered: {len(filtered_records)}")
+
+    return filtered_records
+
+
+def update_dns_records(cf: Cloudflare, records: list, zone_id: str, ip: str):
+    """
+    Updates the DNS records for the specified domain.
+
+    Args:
+        cf (Cloudflare): An instance of the Cloudflare class
+        records (list): A list of DNS records to update
+        zone_id (str): The ID of the Cloudflare zone
+        ip (str): The new IP address to set for the DNS records
+    """
+    for record in records:
+        logging.debug(f"Updating record: {record}")
+        if record.content == ip:
+            logging.info(f"No changes needed for {record.name}")
+            continue
+        try:
+            cf.dns.records.update(
+                dns_record_id=record.id,
+                zone_id=zone_id,
+                content=ip,
+                name=record.name,
+                type=record.type,
+                ttl=record.ttl,
+                comment=f"Updated by rpi-dynamic-dns on {datetime.now()}"
+            )
+        except Exception as e:
+            logging.error(f"Error: {e.reason.strerror}")
+
+
+def main():
+    external_ip = get_public_ip()
+    cf = Cloudflare()
+    dns_records = fetch_dns_records(cf, os.getenv('ZONE_ID'), type='A')
+    update_dns_records(cf, dns_records, os.getenv('ZONE_ID'), ip=external_ip)
+    logging.info("DNS records updated successfully")
+
+
+if __name__ == '__main__':
+    main()
